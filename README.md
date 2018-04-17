bf821a7b0b71e2789e223de49757c14bd6d578eb
ChuckBV <c_s_burks@yahoo.com>
2018-04-17 11:58:58 -0700

    Updated to strip out NA more directly, and loaded up onto Github

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # La_tracker
-Flips a matrix of hours worked from the form in which the data are encountered to the form that we are required to return
+Flips a matrix of hours worked from the form in which the data are encountered to 
+the form that we are required to return. The work is done by "./Software/La_tracker.R"
+
+## In Docs
+
+"La_hr_namelf_y17_pp10.xlsx" is a self-created spreadsheet designed to: 1) be
+organized somewhat similarly to the institutional "WebTA"; 2) facilitate 
+real-time tracking of hours worked to prevent exceeding 1039; and 3) provide
+input into "La_tracker.R"
+
+"La Appt Tracker.xlsx" is a document that admin required to be filed and submitted
+in order to renew a letter of authority (L/A) appointment.
+
+## In Data and Results
+
+"./Data/Hoursin.csv" is obtained from the second tab in the "La_hr_namelf_y17_pp10.xlsx"
+spreadsheet using "save as csv"
+
+"./Results/Hoursout.csv" is opened in Excel for copy-and-paste into "La Appt Tracker.xlsx"
---
+++ Software/La_tracker.R
@@ -0,0 +1,55 @@
+# La_tracker.R
+# Chuck Burks, April 6, 2017
+
+# OBJECTIVE
+# Re-shape work data (hours per day) from the format used 
+# (<= 26 pay periods, 14 days per pay period, 0s included, 
+# so a 26 x 14 (r x c) matrix. The form required from us
+# by "L-A Appt Tracker.xls" is a matrix of 26 rows and 7
+# columns. We are to ignore days with 0 work hours and enter
+# days works serially, days 1-26 in column 1, days 27-52
+# in column 2, etc. The objective of this program is to 
+# automate the reshaping of these data, thereby increasing
+# efficiency and reducing transcription errors.
+
+APROACH
+# Data are collected in an Excel spreadsheet that is an
+# succinct version of the data collected for WebTA (i.e.,
+# 26 rows representing Pay Periods with 14 columns representing
+# the days within the pay periods. The first sheet, "Dataform"
+# is a more human-friendly version that facilitate a running
+# sum of hours in the appointment. The second sheet, "Grid",
+# is a more computer-friendly grid that is saved as a 
+# CSV file and read in by R to reshape the data
+
+#RESHAPING THE DATA
+
+# Read in the grid as a data frame
+hoursin_df <- as.data.frame(read.csv("./Data/Hoursin2.csv", header=TRUE))
+
+# Drop the first two columns (Year and PP)
+hoursin_df$Year <- NULL
+hoursin_df$PP <- NULL
+
+# Convert the remaining 26 x 14 (r x c) grid to a matrix object
+hoursmatrix <- matrix(as.numeric(unlist(hoursin_df)), nrow = nrow(hoursin_df))
+
+# Transpose the matrix from 26 x 14 to 14 x 26. 
+hoursmatrix <-t(hoursmatrix)
+
+# Make the matrix into a vector
+hoursvector <- as.vector(hoursmatrix)
+
+# Drop all elements with a 0 value
+hoursvector <-hoursvector[!is.na(hoursvector)]
+
+# Add however many 0s to the end of the vector to attain a length of 182
+hoursout <- c(hoursvector, rep(0,182-length(hoursvector)))
+
+
+# Reshape to LA tracker format of 7 columns of 26 rows each, 
+# sequentially down then across
+dim(hoursout) <- c(26,7)
+
+# Output as a csv file
+write.table(hoursout, file= "Hoursout.csv", row.names = FALSE, col.names = FALSE, sep = ",")
